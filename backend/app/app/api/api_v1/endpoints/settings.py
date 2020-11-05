4645d2fec076255cb9cbf7e6bf4c3311268b2f28
Tayo Jabar <tayojabar@gmail.com>
2020-11-05 06:40:20 +0200

    generated access token HCO-228

--- backend/app/app/api/api_v1/endpoints/settings.py
+++ backend/app/app/api/api_v1/endpoints/settings.py
@@ -1,6 +1,19 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
+headers = {"accept": "application/json",
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDUyMzI2NjQsInN1YiI6IjEifQ.PrLfAr2v2hu5jb_FFqRUFkq9-flYx6ydYtdXOcWlTfs"
+        }
+
 load_dotenv(find_dotenv())
 env = os.getenv("IDENTITY_DOMAIN__ENV")
 
@@ -14,3 +27,17 @@ if env == "uat":
     url = "https://"+os.getenv("IDENTITY_DOMAIN_UAT_AUTH")+'/api/v1/'
 if env == "production":
     url = "https://"+os.getenv("IDENTITY_DOMAIN_PROD_AUTH")+'/api/v1/'
+
+auth = {"user": os.getenv("FIRST_SUPERUSER"),
+        "password": os.getenv("FIRST_SUPERUSER_PASSWORD")
+    }
+
+payload = {
+        "grant_type": "password",
+        "username": os.getenv("FIRST_SUPERUSER",
+        "password": os.getenv("FIRST_SUPERUSER_PASSWORD")
+      }
+
+token_url = url+'/api/v1/login/access-token'
+resp = session.post(api, payload)
+token = resp.json()["access_token"]
--- backend/app/app/api/api_v1/endpoints/updates.py
+++ backend/app/app/api/api_v1/endpoints/updates.py
@@ -6,14 +6,13 @@ from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
 
-from settings import url
+from settings import (session, headers, url, auth, token)
 
 import requests
 
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 
 ## Get Users
-
 @router.get("/users", response_model=schemas.msg)
 def read_users(
     skip: int = 0,
@@ -23,7 +22,10 @@ def read_users(
     Retrieve users from Identity.
     """
     api = url+'/api/v1/users/?skip='+skip+'&limit='+limit
-    resp = requests.get(api,  
-            auth = HTTPDigestAuth('dev@hivenetwork.online', 'b6e1b95b4014b48746eff931d16fb3d4a53279600c06605f8397d9962e0a6ad9'))
+    resp = session.get(
+                        api,  
+                        auth = HTTPDigestAuth(auth["user"], auth["password"]), 
+                        headers = {"Authorization": token}
+                    )
 
-    return (resp.json)
+    return (resp.json())

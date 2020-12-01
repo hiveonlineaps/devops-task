d55ace98019c05bbdd3486721fc5497f900fe7be
annenamuli <anne.namuli@gmail.com>
2020-12-01 15:44:08 +0200

    HCO-424 removed unused enpoints

--- backend/app/app/api/api_v1/endpoints/transaction.py
+++ backend/app/app/api/api_v1/endpoints/transaction.py
@@ -29,35 +29,18 @@ def read_transactions(
         return transaction
 
 
-@router.post("/{commitment_id}", response_model=schemas.Transaction)
-def create_transaction(
+@router.get("/delivery/{plan_id}/", response_model=List[schemas.Transaction])
+def read_transaction_by_plan_id(
         *,
-        commitment_id: int,
-        db: Session = Depends(deps.get_db),
-        item_in: schemas.TransactionCreate,
-        current_user: models.User = Depends(deps.get_current_active_user),
-) -> Any:
-    """
-    Create new transaction.
-    """
-
-    if current_user:
-        transaction = crud.transaction.create(db=db, obj_in=item_in, commitment_id=commitment_id)
-        return transaction
-
-
-@router.get("/commitment/{deliverer}/", response_model=List[schemas.Transaction])
-def read_commitment_by_deliverer(
-        *,
-        deliverer: int,
+        plan_id: int,
         db: Session = Depends(deps.get_db),
         current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Retrieve Transaction data per deliverer
+    Retrieve production deliveries by plan id
     """
     if current_user:
-        transaction = crud.transaction.get_transactions_by_deliverer(db, deliverer=deliverer)
+        transaction = crud.transaction.get_transaction_by_plan_id(db, plan_id=plan_id)
         return transaction
 
 

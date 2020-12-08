1e2a780335314bc4ebf3f59135652f250522a329
annenamuli <anne.namuli@gmail.com>
2020-12-08 05:52:36 +0200

    merge alembic heads

---
+++ backend/app/alembic/versions/44ab57248daa_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 44ab57248daa
+Revises: fa02c52bf26a, 6a12e66acc5b
+Create Date: 2020-12-08 05:51:53.780925
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '44ab57248daa'
+down_revision = ('fa02c52bf26a', '6a12e66acc5b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
